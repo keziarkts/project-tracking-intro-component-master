4d9c2766d57aeb2757b3bf06122ee90fa00a1340
keziarkts <113933860+keziarkts@users.noreply.github.com>
2023-02-23 14:19:52 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const myHeader = document.querySelector(".nav-wrapper");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const myNav = document.querySelector(".navigation");
+
+navToggle.addEventListener("click", () => {
+    myNav.hasAttribute("data-visible") 
+    ? navToggle.setAttribute("aria-expanded", false) 
+    : navToggle.setAttribute("aria-expanded", true);
+    myNav.toggleAttribute("data-visible");
+    myHeader.toggleAttribute("data-overlay");
+});
